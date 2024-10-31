f8e5910fe1ac1bef41626de600e318c9845a12e3
F4CH <161516529+F4CH@users.noreply.github.com>
2024-10-30 22:08:40 -0300

    Aplicacion de principio OO
    De abierto-Cerrado

--- core/src/main/java/puppy/code/PantallaJuego.java
+++ core/src/main/java/puppy/code/PantallaJuego.java
@@ -1,27 +1,24 @@
 package puppy.code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
-import puppy.code.PowerUps.BalasDiagonales;
-import puppy.code.PowerUps.BalasExtra;
 import puppy.code.PowerUps.PowerUp;
+import puppy.code.PowerUps.BalasExtra;
+import puppy.code.PowerUps.BalasDiagonales;
 import puppy.code.PowerUps.VidasExtra;
 import com.badlogic.gdx.Input;
 
-
 public class PantallaJuego implements Screen {
-
     private SpaceNavigation game;
     private OrthographicCamera camera;
     private SpriteBatch batch;
@@ -33,18 +30,14 @@ public class PantallaJuego implements Screen {
     private float posXNave;
     private float posYNave;
 
-
     private EnemigoBasico1 enemigoBasico1;
     private ArrayList<Enemigo> enemigos = new ArrayList<>();
     private ArrayList<Proyectil> proyectiles = new ArrayList<>();
-
     private Nave4 nave;
-
     private ArrayList<Bullet> balas = new ArrayList<>();
-    /*private BalasExtra auxBalasExtra = new BalasExtra();
-    private VidasExtra auxVidasExtra = new VidasExtra();*/
-    PowerUp powerup;
-    public int getScore() {return score;}
+
+    // Mapa para aplicar PowerUps dinámicamente basado en el puntaje
+    private final Map<Integer, PowerUp> powerUpMap;
 
     public PantallaJuego(SpaceNavigation game, int ronda, int vidas, int score) {
         this.game = game;
@@ -55,31 +48,43 @@ public class PantallaJuego implements Screen {
         batch = game.getBatch();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 640);
-        // Inicializar assets; música de fondo y efectos de sonido
         explosionSound = Gdx.audio.newSound(Gdx.files.internal("explosion.ogg"));
-        explosionSound.setVolume(1,0.5f);
+        explosionSound.setVolume(1, 0.5f);
         gameMusic = Gdx.audio.newMusic(Gdx.files.internal("MusicGame.ogg"));
-
         gameMusic.setLooping(true);
         gameMusic.setVolume(0.5f);
         gameMusic.play();
 
-        // Cargar imagen de la nave, 64x64
-        nave = new Nave4(Gdx.graphics.getWidth()/2-50,30,new Texture(Gdx.files.internal("MainShip3.png")),
+        nave = new Nave4(Gdx.graphics.getWidth() / 2 - 50, 30, new Texture(Gdx.files.internal("MainShip3.png")),
             Gdx.audio.newSound(Gdx.files.internal("hurt.ogg")),
             new Texture(Gdx.files.internal("Rocket2.png")),
             Gdx.audio.newSound(Gdx.files.internal("pop-sound.mp3")));
         nave.setVidas(vidas);
 
-        enemigoBasico1 = new EnemigoBasico1(Gdx.graphics.getWidth()/2-50, 700);
-        EnemigoBasico2 enemigoBasico2 = new EnemigoBasico2(Gdx.graphics.getWidth()/2-150, 700);
-        EnemigoBasico3 enemigoBasico3 = new EnemigoBasico3(Gdx.graphics.getWidth()/2-20, 700);
+        enemigoBasico1 = new EnemigoBasico1(Gdx.graphics.getWidth() / 2 - 50, 700);
+        EnemigoBasico2 enemigoBasico2 = new EnemigoBasico2(Gdx.graphics.getWidth() / 2 - 150, 700);
+        EnemigoBasico3 enemigoBasico3 = new EnemigoBasico3(Gdx.graphics.getWidth() / 2 - 20, 700);
         enemigos.add(enemigoBasico1);
         enemigos.add(enemigoBasico2);
         enemigos.add(enemigoBasico3);
+
+        // Inicializar el mapa de PowerUps
+        powerUpMap = new HashMap<>();
+        powerUpMap.put(50, new BalasExtra());
+        powerUpMap.put(100, new BalasDiagonales());
+        powerUpMap.put(300, new VidasExtra());
     }
 
-    public Nave4 getNave(){return nave;}
+    public Nave4 getNave() {
+        return nave;
+    }
+
+    private void aplicarPowerUpSiCorresponde() {
+        PowerUp powerUp = powerUpMap.get(score);
+        if (powerUp != null) {
+            powerUp.aplicarPowerUp(this);
+        }
+    }
 
     public void dibujaEncabezado() {
         CharSequence str = "Vidas: " + nave.getVidas() + " Ronda: " + ronda;
@@ -91,28 +96,26 @@ public class PantallaJuego implements Screen {
 
     @Override
     public void render(float delta) {
+        if (juegoPausado) return;
 
-        if(juegoPausado) return;
-
-        if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new PantallaPausa(game, this));
             gameMusic.pause();
             juegoPausado = true;
             return;
         }
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         dibujaEncabezado();
 
         nave.draw(batch, this);
 
-
-        for(Enemigo e : enemigos){
+        for (Enemigo e : enemigos) {
             e.draw(batch, this, delta);
         }
 
         // Actualizar y dibujar balas
-
         for (int i = 0; i < balas.size(); i++) {
             Bullet b = balas.get(i);
             b.update();
@@ -121,22 +124,11 @@ public class PantallaJuego implements Screen {
                 Enemigo e = enemigos.get(j);
                 if (e.checkCollision(b)) {
                     b.setDestroyed();
-                    if(e.estaDestruida()) {
+                    if (e.estaDestruida()) {
                         enemigos.remove(j);
                         j--;
-                        score += 300;
-                        if(getScore() % 50 == 0){
-                            powerup = new BalasExtra();
-                            powerup.aplicarPowerUp(this);
-                        }
-                        if(getScore() % 100 == 0){
-                            powerup = new BalasDiagonales();
-                            powerup.aplicarPowerUp(this);
-                        }
-                        if(getScore() % 300 == 0) {
-                            powerup = new VidasExtra();
-                            powerup.aplicarPowerUp(this);
-                        }
+                        score += 50;
+                        aplicarPowerUpSiCorresponde();
                         break;
                     }
                 }
@@ -148,31 +140,28 @@ public class PantallaJuego implements Screen {
         }
 
         // Actualizar proyectiles enemigos
-        for(int i = 0; i < proyectiles.size(); i++){
-            Proyectil p =  proyectiles.get(i);
+        for (int i = 0; i < proyectiles.size(); i++) {
+            Proyectil p = proyectiles.get(i);
             p.update();
             p.draw(batch);
 
-            if(p.isDestroyed()){
+            if (p.isDestroyed()) {
                 proyectiles.remove(i);
                 i--;
             }
         }
 
-        // Dibujar nave y verificar colisiones solo si no está destruida
         nave.draw(batch, this);
         for (int i = 0; i < proyectiles.size(); i++) {
             Proyectil p = proyectiles.get(i);
             p.draw(batch);
 
-            // Verificar colisión solo si la nave no está en estado de vulnerabilidad
             if (!nave.estaHerido() && nave.checkCollision(p)) {
                 proyectiles.remove(i);
                 i--;
             }
         }
 
-        // Verificar si la nave está destruida
         if (nave.estaDestruido()) {
             if (score > game.getHighScore()) {
                 game.setHighScore(score);
@@ -183,7 +172,6 @@ public class PantallaJuego implements Screen {
             dispose();
         }
 
-        // Verificar si el nivel está completo
         if (enemigos.isEmpty()) {
             Screen ss = new PantallaJuego(game, ronda + 1, nave.getVidas(), score);
             ss.resize(1200, 800);
@@ -198,27 +186,28 @@ public class PantallaJuego implements Screen {
         return balas.add(bb);
     }
 
-    public boolean agregarProyectil(Proyectil pp){
+    public boolean agregarProyectil(Proyectil pp) {
         return proyectiles.add(pp);
     }
-
 
     public void reanudarMusica() {
         juegoPausado = false;
         gameMusic.play();
     }
+
     @Override
     public void show() {
         gameMusic.play();
     }
 
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+    }
 
     @Override
     public void pause() {
         juegoPausado = true;
-        if(nave != null){
+        if (nave != null) {
             posXNave = nave.getX();
             posYNave = nave.getY();
             nave.detenerMovimiento();
@@ -226,18 +215,21 @@ public class PantallaJuego implements Screen {
         gameMusic.pause();
         game.setScreen(new PantallaPausa(game, this));
     }
+
     @Override
     public void resume() {
         juegoPausado = false;
-        if(nave != null){
+        if (nave != null) {
             nave.setPosition(posXNave, posYNave);
             nave.detenerMovimiento();
         }
         gameMusic.play();
         game.setScreen(new PantallaJuego(game, ronda, nave.getVidas(), score));
     }
+
     @Override
-    public void hide() {}
+    public void hide() {
+    }
 
     @Override
     public void dispose() {
