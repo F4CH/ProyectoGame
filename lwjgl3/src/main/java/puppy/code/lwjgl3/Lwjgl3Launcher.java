2597c79e7f93ce04f70c08f7f59777260b5d6b9f
JoaquinGarridoRiquelme <joaquin.garrido.r@mail.pucv.cl>
2024-10-31 12:33:52 -0300

    Cambio de ícono

--- lwjgl3/src/main/java/puppy/code/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/puppy/code/lwjgl3/Lwjgl3Launcher.java
@@ -29,7 +29,7 @@ public class Lwjgl3Launcher {
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
         configuration.setWindowedMode(1200, 800);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("Icono_128_bit.png", "Icono_64_bit.png", "Icono_32_bit.png", "Icono_16_bit.png");
         return configuration;
     }
 }
