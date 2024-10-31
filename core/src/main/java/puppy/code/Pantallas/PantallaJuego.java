9853bd35bb87721a53ba35e3f7cb60e22bd23f92
F4CH <161516529+F4CH@users.noreply.github.com>
2024-10-31 17:18:41 -0300

    Modificacion de Score

--- core/src/main/java/puppy/code/Pantallas/PantallaJuego.java
+++ core/src/main/java/puppy/code/Pantallas/PantallaJuego.java
@@ -95,7 +95,7 @@ public class PantallaJuego implements Screen {
         CharSequence str = "Vidas: " + nave.getVidas() + " Ronda: " + ronda;
         game.getFont().getData().setScale(2f);
         game.getFont().draw(batch, str, 10, 30);
-        game.getFont().draw(batch, "Score:" + this.score, Gdx.graphics.getWidth() - 150, 30);
+        game.getFont().draw(batch, "Score:" + this.score, Gdx.graphics.getWidth() - 200, 30);
         game.getFont().draw(batch, "HighScore:" + game.getHighScore(), Gdx.graphics.getWidth() / 2 - 100, 30);
     }
 
