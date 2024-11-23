35a3a8c9aaf7f0fb709ab511a08c8f9c36ce17c9
F4CH <161516529+F4CH@users.noreply.github.com>
2024-11-23 16:29:14 -0300

    Return de Builder

--- core/src/main/java/puppy/code/Patrones/OleadaBuilder.java
+++
@@ -1,9 +0,0 @@
-package puppy.code.Patrones;
-
-public interface OleadaBuilder {
-    void configurarNivel(int nivel);
-    void agregarEnemigoBasico1(int cantidad);
-    void agregarEnemigoBasico2(int cantidad);
-    void agregarEnemigoBasico3(int cantidad);
-    OleadaProducto getProducto();
-}
